8795409375c98f5f0bad6aad434c057acff8c5e7
mathewnithin353 <mathewnithin353@gmail.com>
2025-11-15 14:39:36 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+print("current date and time:", datetime.datetime.now())
+
+print("current year:",
+datetime.date.today().strftime("%Y"))
+print("month of year:",
+datetime.date.today().strftime("%B"))
+
+print("week number of the year",
+datetime.date.today().strftime("%W"))
+print("Day number of the week:",
+datetime.date.today().strftime("%w"))
+
+print("day of year:", datetime.date.today().strftime("%j"))
+print("day of the month:",
+datetime.date.today().strftime("%d"))
+print("day of week", datetime.date.today().strftime("%A"))
+print("\nUsing time module:")
+current_time = time.localtime()
+print("Today’s date:", time.strftime("%Y-%m-%d",
+current_time))
